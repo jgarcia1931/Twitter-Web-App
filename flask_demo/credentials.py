2a3ab19ffa4d4a0c47a7cf5351d0b4a921c3a231
Jose M <Jose.m.garcia1931@gmail.com>
2019-01-31 20:29:44 -0600

    Update credentials.py

--- flask_demo/credentials.py
+++ flask_demo/credentials.py
@@ -6,10 +6,10 @@
 # Consume:
 
 
-CONSUMER_KEY    = 'wbdVCOhmB1Gw8oUSTVen19RdP'
-CONSUMER_SECRET = 'oOtj03fLhNBeb4AqxTeGGN76cpOb1iiqj59H2eNli8B737Le8D'
-ACCESS_TOKEN    = '881626873-47GUciN7LWOGAECX7xKphUk9IaAX71ODnv8ikcFj'
-ACCESS_SECRET   = 'MwlOmN68UrmEJMOIQSTH7jqtp37Zv1JN3sjQxegw1ve8c'
+CONSUMER_KEY    = '########################'
+CONSUMER_SECRET = '########################'
+ACCESS_TOKEN    = '########################'
+ACCESS_SECRET   = '########################'
 
 
 
